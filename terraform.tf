a80c3fec166470cee24bef3eb2940d399181d7ca
MOHAMMADREZA <zeys@0a:25:3d:5f:1f:12.home>
2026-09-15 21:31:55 -0400

    deploy

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.92"
     }
   }
+  backend "s3" {
+    bucket = "terraform-state-sep14-2026"
+    key    = "env/dev/terraform-base.tfstate"
+    region = "ca-central-1"
+  }
 
   required_version = ">= 1.2"
 }
